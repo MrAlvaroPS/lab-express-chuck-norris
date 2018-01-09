b1ffdd4b3bfd97e596b7d3f19992618347379d63
Alvaro Pompa <alvaropompa@gmail.com>
2018-01-09 21:22:00 +0100

    probando el express :)

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const app = express();
-const Chuck  = require('chucknorris-io');
+const Chuck = require('chucknorris-io');
 const client = new Chuck();
 
+
+client.getRandomJoke()
+  .then((response) => {
+    console.log("jorl");
+  }).catch((err) => {
+    // handle error
+  });
+
+
+
+app.get('/', function(req, res) {
+  res.send(getRandomJoke());
+});
+
+
+app.listen(3000, function() {
+  console.log("listening")
+});
